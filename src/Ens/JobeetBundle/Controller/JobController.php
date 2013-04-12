33c9480a5cde54b779b0f9520dcef4b1ad6127c2
Alexandre PAIXAO <alexandre.paixao@stadline.com>
2013-04-12 16:50:17 +0200

    Changed "post a job" button link

--- src/Ens/JobeetBundle/Controller/JobController.php
+++ src/Ens/JobeetBundle/Controller/JobController.php
@@ -50,7 +50,12 @@ class JobController extends Controller
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('ens_job_show', array('id' => $entity->getId())));
+            return $this->redirect($this->generateUrl('ens_job_show', array(
+                'company' => $entity->getCompanySlug(),
+                'location' => $entity->getLocationSlug(),
+                'id' => $entity->getId(),
+                'position' => $entity->getPositionSlug()
+            )));
         }
 
         return $this->render('EnsJobeetBundle:Job:new.html.twig', array(
