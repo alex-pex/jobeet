a5c1ab1cf71c198bbabd51e13f1e5d48cae071e4
Alexandre PAIXAO <alexandre.paixao@stadline.com>
2013-04-15 17:12:04 +0200

    Added some tests on Job form

--- src/Ens/JobeetBundle/Form/JobType.php
+++ src/Ens/JobeetBundle/Form/JobType.php
@@ -27,6 +27,6 @@ class JobType extends AbstractType
     
     public function getName()
     {
-        return 'ens_jobeetbundle_jobtype';
+        return 'job';
     }
 }
--- src/Ens/JobeetBundle/Tests/Controller/JobControllerTest.php
+++ src/Ens/JobeetBundle/Tests/Controller/JobControllerTest.php
@@ -54,7 +54,7 @@ class JobControllerTest extends WebTestCase
         $this->assertTrue($crawler->filter('.category_design .more_jobs')->count() == 0);
         $this->assertTrue($crawler->filter('.category_programming .more_jobs')->count() == 1);
         
-        // jobs are sorted by date
+        // jobs are sorted by datearray_mergearray_merge
         $this->assertTrue($crawler->filter('.category_programming tr')->first()->filter(sprintf('a[href*="/%d/"]', $this->getMostRecentProgrammingJob()->getId()))->count() == 1);
         
         // each job on the homepage is clickable and give detailed information
@@ -74,5 +74,62 @@ class JobControllerTest extends WebTestCase
         // an expired job page forwards the user to a 404
         $crawler = $client->request('GET', sprintf('/job/sensio-labs/paris-france/%d/web-developer-expired', $this->getExpiredJob()->getId()));
         $this->assertTrue(404 === $client->getResponse()->getStatusCode());
+    }
+    
+    public function testJobForm()
+    {
+        // test with good values
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/job/new');
+        $this->assertEquals('Ens\JobeetBundle\Controller\JobController::newAction', $client->getRequest()->attributes->get('_controller'));
+        
+        $form = $crawler->selectButton('Preview your job')->form(array(
+            'job[company]' => 'Sensio Labs',
+            'job[url]' => 'http://www.sensio.com/',
+            'job[file]' => __DIR__.'/../../../../../web/bundles/ensjobeet/images/sensio-labs.gif',
+            'job[position]' => 'Developer',
+            'job[location]' => 'Atlanta, USA',
+            'job[description]' => 'You will work with symfony to develop websites for our customers.',
+            'job[how_to_apply]' => 'Send me an email',
+            'job[email]' => 'for.a.job@example.com',
+            'job[is_public]' => false,
+        ));
+        
+        $client->submit($form);
+        $this->assertEquals('Ens\JobeetBundle\Controller\JobController::createAction', $client->getRequest()->attributes->get('_controller'));
+        
+        $client->followRedirect();
+        $this->assertEquals('Ens\JobeetBundle\Controller\JobController::previewAction', $client->getRequest()->attributes->get('_controller'));
+        
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+        
+        $query = $em->createQuery('SELECT count(j.id) from EnsJobeetBundle:Job j WHERE j.location = :location AND j.is_activated IS NULL AND j.is_public = 0');
+        $query->setParameter('location', 'Atlanta, USA');
+        $this->assertTrue(0 < $query->getSingleScalarResult());
+        
+        // test with bad values
+        $crawler = $client->request('GET', '/job/new');
+        $form = $crawler->selectButton('Preview your job')->form(array(
+                'job[company]' => 'Sensio Labs',
+                'job[position]' => 'Developer',
+                'job[location]' => 'Atlanta, USA',
+                'job[email]' => 'not.an.email',
+        ));
+        $crawler = $client->submit($form);
+        
+        // check if we have 3 errors
+        $this->assertTrue($crawler->filter('.error_list')->count() == 3);
+        
+        // check if we have error on job_description field
+        $this->assertTrue($crawler->filter('#job_description')->siblings()->first()->filter('.error_list')->count() == 1);
+        
+        // check if we have error on job_how_to_apply field
+        $this->assertTrue($crawler->filter('#job_how_to_apply')->siblings()->first()->filter('.error_list')->count() == 1);
+        
+        // check if we have error on job_email field
+        $this->assertTrue($crawler->filter('#job_email')->siblings()->first()->filter('.error_list')->count() == 1);
     }
 }
