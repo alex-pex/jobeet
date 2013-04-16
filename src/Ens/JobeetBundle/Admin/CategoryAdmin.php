47ee77e661dd1ebc5d710a6b97bd1224bf7e2fda
Alexandre PAIXAO <alexandre.paixao@stadline.com>
2013-04-16 11:24:44 +0200

    Registered admin pages @see http://symfony.com/doc/2.0/book/service_container.html

---
+++ src/Ens/JobeetBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ens\JobeetBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CategoryAdmin extends Admin
+{
+    
+}
---
+++ src/Ens/JobeetBundle/Controller/JobAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ens\JobeetBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+class JobAdminController extends Controller
+{
+    
+}
