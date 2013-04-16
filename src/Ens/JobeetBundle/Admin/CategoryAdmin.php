5be77bc633dc2e14da320a5b01cd738f61b9d962
Alexandre PAIXAO <alexandre.paixao@stadline.com>
2013-04-16 15:24:39 +0200

    Updated Category and Job admin pages

--- src/Ens/JobeetBundle/Admin/CategoryAdmin.php
+++ src/Ens/JobeetBundle/Admin/CategoryAdmin.php
@@ -3,12 +3,41 @@
 namespace Ens\JobeetBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
-use Sonata\AdminBundle\Datagrid\ListMapper;
-use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+use Ens\JobeetBundle\Entity\Category;
 
 class CategoryAdmin extends Admin
 {
+    // setup the default sort column and order
+    protected $datagridValues = array(
+        '_sort_by'    => 'name',
+        '_sort_order' => 'ASC',
+    );
     
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+          ->add('name')
+          ->add('slug')
+        ;
+    }
+    
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+          ->add('name')
+        ;
+    }
+    
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+          ->addIdentifier('name')
+          ->add('slug')
+        ;
+    }
 }
