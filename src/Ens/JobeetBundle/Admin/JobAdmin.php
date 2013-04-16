d6be517d90e2c8979ae8d7d5d24a8018c871c5e8
Alexandre PAIXAO <alexandre.paixao@stadline.com>
2013-04-16 16:46:29 +0200

    Added batch action to Job admin page

--- src/Ens/JobeetBundle/Admin/JobAdmin.php
+++ src/Ens/JobeetBundle/Admin/JobAdmin.php
@@ -90,4 +90,25 @@ class JobAdmin extends Admin
           ->add('expires_at')
         ;
     }
+    
+    public function getBatchActions()
+    {
+        // retrieve the default (currently only the delete action) actions
+        $actions = parent::getBatchActions();
+        
+        // check user permissions
+        if($this->hasRoute('edit') && $this->isGranted('EDIT') && $this->hasRoute('delete') && $this->isGranted('DELETE')){
+            $actions['extend'] = array(
+                'label' => 'Extend',
+                'ask_confirmation' => true // If true, a confirmation will be asked before performing the action
+            );
+            
+            $actions['deleteNeverActivated'] = array(
+                'label' => 'Delete never activated jobs',
+                'ask_confirmation' => true // If true, a confirmation will be asked before performing the action
+            );
+        }
+        
+        return $actions;
+    }
 }
